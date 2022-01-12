b056a7d428e624692bec0a46c2ed5ea9700e008c
veda <veda@train>
2022-01-12 12:06:09 +0530

    first commit

---
+++ src/services/AuthService.ts
@@ -0,0 +1,17 @@
+import { User } from "../model/Model";
+
+export class AuthService {
+  public async login(
+    userName: string,
+    password: string
+  ): Promise<User | undefined> {
+    if (userName === "user" && password === "1234") {
+      return {
+        userName: userName,
+        email: "some@gmail.com",
+      };
+    } else {
+      return undefined;
+    }
+  }
+}
